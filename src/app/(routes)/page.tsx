5cd42825cf9692819261a4ef7dc7fe05e4e16703
Chankuri2049 <godchestnut1208@gmail.com>
2024-12-01 08:26:12 +0900

    ディレクトリ構成の設定およびshadcn-uiの導入

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -48,6 +48,8 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+
+
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
